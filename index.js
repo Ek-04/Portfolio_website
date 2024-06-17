0253cb097f9e6f58a470721bc46fdc3d2366d1d3
Ek-04 <wkariuki.e@gmail.com>
2024-06-17 15:57:41 +0300

    commit2

---
+++ index.js
@@ -0,0 +1,12 @@
+var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+        function opentab(tabname){
+            for(var tablink of tablinks){
+                tablink.classList.remove("active-link");
+            }
+            for(var tabcontent of tabcontents){
+                tabcontent.classList.remove("active-tab");
+            }
+            document.getElementById(tabname).classList.add("active-tab");
+            event.currentTarget.classList.add("active-link");
+        }
